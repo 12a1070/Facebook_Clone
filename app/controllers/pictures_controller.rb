7e5dcf0f22c221edb868db6cf9b459b9bc15d7e2
teraitomoya <12a1070.sit.shonan@gmail.com>
2021-08-29 16:08:56 +0900

    “エラー機能”

---
+++ app/controllers/pictures_controller.rb
@@ -0,0 +1,24 @@
+class PicturesController < ApplicationController
+
+
+  def index
+    @pictures = Picture.ApplicationController
+  end
+
+  def confirm
+    @picture = current_user.pictures.build(picture_params)
+    render :new if @picture.invalid?
+  end
+
+  def create
+    @picture = current_user.pictures.build(picture_params)
+
+
+
+
+
+
+
+
+
+end
